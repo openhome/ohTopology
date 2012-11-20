dd670196973336c448ff484fead6a685d87bd511
David Drysdale-Wilson <david.drysdale-wilson@linn.co.uk>
2012-11-20 14:42:19 +0000

    Added start of topology2 interface. Added start of TestTopology1.

---
+++ src/TestTopology1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+using OpenHome.Os.App;
+using OpenHome.Av;
+using OpenHome.Net.ControlPoint;
+using OpenHome.Net.Core;
+
+namespace TestTopology
+{
+    class Program
+    {
+        class ExceptionReporter : IExceptionReporter
+        {
+            public void ReportException(Exception e)
+            {
+                Console.WriteLine(e);
+                Environment.Exit(-1);
+            }
+        }
+
+        class DeviceWatcher : ICollectionWatcher<IWatchableDevice>
+        {
+
+            public void CollectionAdd(IWatchableDevice aItem, uint aIndex)
+            {
+                Console.WriteLine("Collection Add at " + aIndex);
+            }
+
+            public void CollectionClose()
+            {
+                Console.WriteLine("Collection Close");
+            }
+
+            public void CollectionInitialised()
+            {
+                Console.WriteLine("Collection Initialised");
+            }
+
+            public void CollectionMove(IWatchableDevice aItem, uint aFrom, uint aTo)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void CollectionOpen()
+            {
+                Console.WriteLine("Collection Open");
+            }
+
+            public void CollectionRemove(IWatchableDevice aItem, uint aIndex)
+            {
+                Console.WriteLine("Collection Remove at " + aIndex);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            InitParams initParams = new InitParams();
+            Library library = Library.Create(initParams);
+
+            SubnetList subnets = new SubnetList();
+            library.StartCp(subnets.SubnetAt(0).Subnet());
+            subnets.Dispose();
+
+            ExceptionReporter reporter = new ExceptionReporter();
+            WatchableThread thread = new  WatchableThread(reporter);
+            Topology1 topology = new Topology1(thread);
+
+            DeviceWatcher watcher = new DeviceWatcher();
+            topology.Devices.AddWatcher(watcher);
+
+            System.Threading.Thread.Sleep(10000);
+
+            topology.Devices.RemoveWatcher(watcher);
+            
+            topology.Dispose();
+
+            thread.Dispose();
+
+            library.Dispose();
+        }
+    }
+}
---
+++ src/ohTopology/Topology2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+using OpenHome.Os.App;
+using OpenHome.Net.ControlPoint;
+
+namespace OpenHome.Av
+{
+    public class Topology2 : ICollectionWatcher<IWatchableDevice>, IDisposable
+    {
+        public Topology2(WatchableThread aThread, ITopology1 aTopology1)
+        {
+            iDisposed = false;
+            iLock = new object();
+
+            iThread = aThread;
+            iTopology1 = aTopology1;
+
+            iTopology1.Devices.AddWatcher(this);
+        }
+
+        public void Dispose()
+        {
+            lock (iLock)
+            {
+                if (iDisposed)
+                {
+                    throw new ObjectDisposedException("Topology2.Dispose");
+                }
+
+                iTopology1.Dispose();
+
+                iDisposed = true;
+            }
+        }
+
+        public void CollectionAdd(IWatchableDevice aItem, uint aIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CollectionClose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CollectionInitialised()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CollectionMove(IWatchableDevice aItem, uint aFrom, uint aTo)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void CollectionOpen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CollectionRemove(IWatchableDevice aItem, uint aIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        private object iLock;
+        private bool iDisposed;
+
+        private WatchableThread iThread;
+        private ITopology1 iTopology1;
+    }
+}
