128b4f362d2a1fcc1d22867f4e52df274006d4b1
David Drysdale-Wilson <david.drysdale-wilson@linn.co.uk>
2012-12-05 14:46:21 +0000

    Added required file.

---
+++ src/ohTopology/Topology3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OpenHome.Av
+{
+    public interface ITopology3
+    {
+    }
+
+    public class Topology3 : ITopology3
+    {
+        public Topology3(ITopology2 aTopology2)
+        {
+        }
+    }
+}
