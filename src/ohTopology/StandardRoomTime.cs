33da9f8cf41ba1628191e84a6457fc68bad04b67
David Drysdale-Wilson <david.drysdale-wilson@linn.co.uk>
2013-05-21 09:59:49 +0100

    Check in project file that references new service file names.

---
+++ src/ohTopology/StandardRoomTime.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using OpenHome.Os.App;
+
+namespace OpenHome.Av
+{
+    public class StandardRoomTime : IDisposable
+    {
+        public StandardRoomTime(IStandardRoom aRoom)
+        {
+            iLock = new object();
+            iDisposed = false;
+            iThread = aRoom.WatchableThread;
+            iRoom = aRoom;
+
+            iRoom.Join(SetInactive);
+
+            Task<ProxyTime> task = null;// iRoom.Time.Create<ProxyTime>();
+            iThread.Schedule(() =>
+            {
+                ProxyTime time = task.Result;
+
+                lock(iLock)
+                {
+                    if (!iDisposed)
+                    {
+                        //iTime = time;
+                    }
+                    else
+                    {
+                        time.Dispose();
+                    }
+                }
+            });
+        }
+
+        public void Dispose()
+        {
+            lock (iLock)
+            {
+                if (iIsActive)
+                {
+                    iDuration.Detach();
+                    iSeconds.Detach();
+
+                    iRoom.UnJoin(SetInactive);
+                }
+
+                iRoom = null;
+
+                iActive.Dispose();
+                iActive = null;
+
+                iDisposed = true;
+            }
+        }
+
+        private void SetInactive()
+        {
+            lock (iLock)
+            {
+                iIsActive = false;
+
+                iActive.Update(false);
+
+                iDuration.Detach();
+                iSeconds.Detach();
+
+                iRoom.UnJoin(SetInactive);
+            }
+        }
+
+        public IWatchable<bool> Active
+        {
+            get
+            {
+                return iActive;
+            }
+        }
+
+        public IWatchable<uint> Duration
+        {
+            get
+            {
+                return iDuration;
+            }
+        }
+
+        public IWatchable<uint> Seconds
+        {
+            get
+            {
+                return iSeconds;
+            }
+        }
+
+        private bool iDisposed;
+
+        private IWatchableThread iThread;
+        private IStandardRoom iRoom;
+
+        private object iLock;
+        private bool iIsActive;
+        private Watchable<bool> iActive;
+
+        private WatchableProxy<uint> iDuration;
+        private WatchableProxy<uint> iSeconds;
+    }
+}
