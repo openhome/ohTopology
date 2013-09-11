82678d80d50bec3628c07088b945a9479edf8053
Graham Darnell <graham.darnell@linn.co.uk>
2013-09-11 08:37:23 +0100

    Improved media supervisor test program

--- src/TestMediaEndpoint/Program.cs
+++ src/TestMediaEndpoint/Program.cs
@@ -296,6 +296,8 @@ namespace TestMediaEndpoint
                             });
                         }));
                     });
+
+                    Thread.Sleep(aMilliseconds);
                 }
 
                 client.Wait();
