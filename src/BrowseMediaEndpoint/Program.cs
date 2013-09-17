4b67d497edb0f700cfde05c251e601dcee41a4e3
Graham Darnell <graham.darnell@linn.co.uk>
2013-09-17 13:43:13 +0100

    Added manual test program BrowseMediaEndpoint

---
+++ src/BrowseMediaEndpoint/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenHome;
+using OpenHome.Net.Core;
+
+namespace BrowseMediaEndpoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var initParams = new OpenHome.Net.Core.InitParams();
+            var library = Library.Create(initParams);
+
+            var adapters = new List<NetworkAdapter>();
+
+            using (var subnets = new SubnetList())
+            {
+                var count = subnets.Size();
+
+                if (count == 0)
+                {
+                    Console.WriteLine("No network adapter");
+                    return;
+                }
+
+                var main = new Main(library, subnets.SubnetAt(0));
+
+                main.Run();
+            }
+        }
+    }
+}
